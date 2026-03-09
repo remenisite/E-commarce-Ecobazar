0a5b8b70b31b39238016cc58b6d1a6549d749600
antor94 <antorremeni@gmail.com>
2026-03-09 22:54:48 +0600

    change code

--- client/app/components/layout/Header.js
+++ client/app/components/layout/Header.js
@@ -1,39 +1,85 @@
 import { FaMapMarkerAlt } from "react-icons/fa";
 import { MdKeyboardArrowDown } from "react-icons/md";
+import { FaHeart, FaShoppingBag, FaPhoneAlt, FaRegHeart } from "react-icons/fa";
+
+import { IoSearchOutline } from "react-icons/io5";
+import { HiOutlineShoppingBag } from "react-icons/hi";
+import { FiPhoneCall } from "react-icons/fi";
+import Image from "next/image";
+import Link from "next/link";
+import { FiMapPin } from "react-icons/fi";
+
 
 const Header = () => {
   return (
-    <header className="w-full py-[12px] bg-[#333333] text-[12px] font-normal font-main text-[#B3B3B3]">
+
+    <>
+    
+    <header className="w-full py-[12px] bg-[#EDF2EE] text-[12px] font-normal font-main text-[#B3B3B3]">
       <div className="container flex items-center justify-between">
         
         {/* Left Section */}
         <div className="flex items-center gap-2">
-          <FaMapMarkerAlt className="text-gray-300" />
-          <span className="hidden sm:inline">
+          <FiMapPin className="text-[16px] text-green" />
+          <span className="hidden sm:inline text-[15px] font-normal font-main text-green">
             Store Location: Lincoln- 344, Illinois, Chicago, USA
           </span>
         </div>
 
         {/* Right Section */}
         <div className="flex items-center gap-4">
-          <div className="flex items-center gap-1 cursor-pointer">
-            <span>Eng</span>
-            <MdKeyboardArrowDown />
-          </div>
 
-          <div className="flex items-center gap-1 cursor-pointer">
-            <span>USD</span>
-            <MdKeyboardArrowDown />
-          </div>
+          <select className="px-4 py-2 text-[15px] font-normal font-main text-green">
+          <option value="" className="text-[15px] font-normal font-main text-green">Eng</option>
+          <option value="" className="text-[15px] font-normal font-main text-green">bangla</option>
+          <option value="" className="text-[15px] font-normal font-main text-green">Hindi</option>
+          <option value="" className="text-[15px] font-normal font-main text-green">Urdu</option>
+        </select>
 
-          <span className="cursor-pointer ">
-            <a href="/signin" className=" hover:text-white">Sign In</a>/
-            <a href="/signup" className=" hover:text-white">Sign Up</a>
-              
-          </span>
+
+        
+
         </div>
       </div>
     </header>
+    
+
+          <div className="container flex items-center justify-between py-[27px] gap-4">
+            <Link href="/"><Image src="/images/main-logo.png"width={200}  height={200} alt="navbar-logo" /></Link>
+            <div className="hidden rounded-[6px] md:flex border border-gray-300 flex-1 max-w-xl mx-6">
+              <IoSearchOutline className="text-[20px] " />
+              <input
+                type="text"
+                placeholder="Search"
+                className="w-full  text-[15px] font-normal font-main placeholder:text-[#808080] px-4 py-2 rounded-l-md outline-none"
+              />
+              <button className="bg-green text-[14px] font-semibold font-main text-white px-6 rounded-r-[6px]">
+                Search
+              </button>
+            </div>
+            <div className="flex items-center gap-6">
+              <Link href={"wishList"}>
+                <FaRegHeart className="text-[32px] cursor-pointer" />
+              </Link>
+              <div className="relative cursor-pointer">
+                <Link href={"shoppingCart"}>
+                  <HiOutlineShoppingBag className="text-[32px] cursor-pointer" />
+                </Link>
+                <span className="absolute -top-2 -right-2 bg-green text-white text-xs w-5 h-5 flex items-center justify-center rounded-full"> 2  </span>
+              </div>
+              <div className="hidden md:block text-[11px] font-normal font-main">
+                <p className="text-[#4D4D4D]">Shopping cart:</p>
+                <p className="text-[14px] font-medium font-main text-text">
+                  $57.00
+                </p>
+              </div>
+            </div>
+          </div>
+
+
+
+    
+    </>
   );
 };
 
--- client/app/components/layout/Navbar.js
+++ client/app/components/layout/Navbar.js
@@ -1,107 +1,72 @@
 "Use client";
 
 import React from "react";
-import { FaHeart, FaShoppingBag, FaPhoneAlt, FaRegHeart } from "react-icons/fa";
-import { MdKeyboardArrowDown } from "react-icons/md";
 
-import { IoSearchOutline } from "react-icons/io5";
-import { HiOutlineShoppingBag } from "react-icons/hi";
-import { FiPhoneCall } from "react-icons/fi";
-import Image from "next/image";
 import Link from "next/link";
+import { MdKeyboardArrowDown } from "react-icons/md";
+import { FiPhoneCall } from "react-icons/fi";
 
 const Navbar = () => {
   return (
     <>
       <nav className="w-full bg-white border-b">
         <div className="container">
-          <div className="container flex items-center justify-between py-[27px] gap-4">
-            <Link href="/"><Image src="/images/main-logo.png"width={200}  height={200} alt="navbar-logo" /></Link>
-            <div className="hidden rounded-[6px] md:flex border border-gray-300 flex-1 max-w-xl mx-6">
-              <IoSearchOutline className="text-[20px] " />
-              <input
-                type="text"
-                placeholder="Search"
-                className="w-full  text-[15px] font-normal font-main placeholder:text-[#808080] px-4 py-2 rounded-l-md outline-none"
-              />
-              <button className="bg-green text-[14px] font-semibold font-main text-white px-6 rounded-r-[6px]">
-                Search
-              </button>
+          <nav className="  hidden md:flex justify-between py-[20px] items-center gap-6 font-medium">
+            <div className="flex items-center">
+              <ul className="flex items-center justify-center gap-[32px]">
+                <li>
+                  <Link
+                    href={"/"}
+                    className="text-[16px] font-medium font-main text-[#666666] flex items-center gap-1"
+                  >
+                    Home <MdKeyboardArrowDown />
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href={"/shop"}
+                    className="text-[14px] font-medium font-main text-[#666666] flex items-center gap-1"
+                  >
+                    Shop <MdKeyboardArrowDown />
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href={"/checkout"}
+                    className="text-[14px] font-medium font-main text-[#666666] flex items-center gap-1"
+                  >
+                    {" "}
+                    Pages <MdKeyboardArrowDown />
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href={"/"}
+                    className="text-[14px] font-medium font-main text-[#666666] flex items-center gap-1"
+                  >
+                    {" "}
+                    Blog <MdKeyboardArrowDown />
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href={"/about"}
+                    className="text-[14px] font-medium font-main text-[#666666] flex items-center gap-1"
+                  >
+                    {" "}
+                    About Us
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href={"/contact"}
+                    className="text-[14px] font-medium font-main text-[#666666] flex items-center gap-1"
+                  >
+                    Contact Us
+                  </Link>
+                </li>
+              </ul>
             </div>
-            <div className="flex items-center gap-6">
-              <Link href={"wishList"}>
-                <FaRegHeart className="text-[32px] cursor-pointer" />
-              </Link>
-              <div className="relative cursor-pointer">
-                <Link href={"shoppingCart"}>
-                  <HiOutlineShoppingBag className="text-[32px] cursor-pointer" />
-                </Link>
-                <span className="absolute -top-2 -right-2 bg-green text-white text-xs w-5 h-5 flex items-center justify-center rounded-full"> 2  </span>
-              </div>
-              <div className="hidden md:block text-[11px] font-normal font-main">
-                <p className="text-[#4D4D4D]">Shopping cart:</p>
-                <p className="text-[14px] font-medium font-main text-text">
-                  $57.00
-                </p>
-              </div>
-            </div>
-          </div>
-            <nav className="  hidden md:flex justify-between py-[20px] items-center gap-6 font-medium">
-              <div className="flex items-center">
-                <ul className="flex items-center justify-center gap-[32px]">
-                  <li>
-                    <Link
-                      href={"/"}
-                      className="text-[16px] font-medium font-main text-[#666666] flex items-center gap-1"
-                    >
-                      Home <MdKeyboardArrowDown />
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      href={"/shop"}
-                      className="text-[14px] font-medium font-main text-[#666666] flex items-center gap-1"
-                    >
-                      Shop <MdKeyboardArrowDown />
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      href={"/checkout"}
-                      className="text-[14px] font-medium font-main text-[#666666] flex items-center gap-1"
-                    >
-                      {" "}
-                      Pages <MdKeyboardArrowDown />
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      href={"/"}
-                      className="text-[14px] font-medium font-main text-[#666666] flex items-center gap-1"
-                    >
-                      {" "}
-                      Blog <MdKeyboardArrowDown />
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      href={"/about"}
-                      className="text-[14px] font-medium font-main text-[#666666] flex items-center gap-1"
-                    >
-                      {" "}
-                      About Us
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      href={"/contact"}
-                      className="text-[14px] font-medium font-main text-[#666666] flex items-center gap-1"
-                    >
-                      Contact Us
-                    </Link>
-                  </li>
-                </ul>
-              </div>
 
             <div className="flex items-center gap-2 font-medium">
               <FiPhoneCall className="text-[28px] text-text" />
@@ -109,8 +74,7 @@ const Navbar = () => {
                 (219) 555-0114
               </span>
             </div>
-
-            </nav>
+          </nav>
         </div>
       </nav>
     </>
