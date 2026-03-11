ca406c70372e7a072dbccbe4d1e9b832fe27b078
antor94 <antorremeni@gmail.com>
2026-03-11 23:29:51 +0600

    change banner

--- client/app/components/home/banner/HeroSection.jsx
+++ client/app/components/home/banner/HeroSection.jsx
@@ -1,11 +1,17 @@
 "use client";
+
 import Image from "next/image";
-import { FaArrowRight } from "react-icons/fa";
+import heroImg from "../../../../public/images/heroImg.png"
 
 export default function HeroSection() {
   return (
     <section id="banner" className="">
 
+      <div container>
+        <Image src={heroImg} alt="heroImg"  width={742} height={498}/>
+      </div>
+
+  
     
     </section>
   );
