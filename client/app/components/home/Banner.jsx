0356503d7e1cd656754dd3a839a37c8dbb0293d2
antor94 <antorremeni@gmail.com>
2026-03-11 23:39:58 +0600

    change code

--- client/app/components/home/Banner.jsx
+++ client/app/components/home/Banner.jsx
@@ -1,11 +1,32 @@
-import { FaShoppingCart, FaLeaf, FaTags } from "react-icons/fa";
-import HeroSection from "./banner/HeroSection";
+"use client";
+import { GoArrowRight } from "react-icons/go";
+import Image from "next/image";
+import heroImg from "../../../public/images/heroImg.png"
+import Button from "../ui/Button";
+
+
 
 const Banner = () => {
   return (
-    <div className="my-[100px] container">
-      <HeroSection />
-    </div>
+       <section id="banner" className="py-[72px]">
+
+      <div className="container flex justify-between">
+        <Image src={heroImg} alt="heroImg"  width={742} height={498}/>
+        <div>
+          <h5 className="text-[14px] font-medium font-main text-green">Welcome to shopery</h5>
+          <h1 className="mt-[8px] mb-[28px] text-[72px] font-semibold font-main text-text">Fresh & Healthy <br /> Organic Food</h1>
+          <h3 className="text-[32px] font-normal font-main text-text">Sale up to <span className="text-[#FF8A00] font-semibold">30% OFF</span></h3>
+          <p className="mt-[12px] mb-[32px] text-[14px] font-mormal font-main text-[#808080]">Free shipping on all your order. we deliver, you enjoy</p>
+          <div className="w-[190px]">
+          <Button >Shop now <GoArrowRight /></Button>
+
+          </div>
+        </div>
+      </div>
+
+  
+    
+    </section>
   );
 };
 
--- client/app/components/home/banner/HeroSection.jsx
+++ client/app/components/home/banner/HeroSection.jsx
@@ -1,4 +1,6 @@
 "use client";
+import { GoArrowRight } from "react-icons/go";
+
 
 import Image from "next/image";
 import heroImg from "../../../../public/images/heroImg.png"
@@ -6,16 +8,16 @@ import Button from "../../ui/Button";
 
 export default function HeroSection() {
   return (
-    <section id="banner" className="">
+    <section id="banner" className="py-[72px]">
 
-      <div container>
+      <div className="container flex">
         <Image src={heroImg} alt="heroImg"  width={742} height={498}/>
         <div>
           <h5 className="text-[14px] font-medium font-main text-green">Welcome to shopery</h5>
-          <h1 className="text-[72px] font-semibold font-main text-text">Fresh & Healthy <br /> Organic Food</h1>
+          <h1 className="mt-[8px] mb-[28px] text-[72px] font-semibold font-main text-text">Fresh & Healthy <br /> Organic Food</h1>
           <h3 className="text-[32px] font-normal font-main text-text">Sale up to <span className="text-[#FF8A00] font-semibold">30% OFF</span></h3>
-          <p className="text-[14px] font-mormal font-main text-[#808080]">Free shipping on all your order. we deliver, you enjoy</p>
-          <Button >Shop now</Button>
+          <p className="mt-[12px] mb-[32px] text-[14px] font-mormal font-main text-[#808080]">Free shipping on all your order. we deliver, you enjoy</p>
+          <Button >Shop now <GoArrowRight /></Button>
         </div>
       </div>
 
