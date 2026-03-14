d10c097616e4470ded2f7d9d4b91f4527b00e92d
antor94 <antorremeni@gmail.com>
2026-03-14 17:28:58 +0600

    change code

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router";
+import MainLayout from "./layout/MainLayout";
+import Dashboard from "./pages/Dashboard";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainLayout />}>
+            <Route index element={<Dashboard />}></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+};
 
-export default App
+export default App;
---
+++ admin/src/layout/MainLayout.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Outlet } from 'react-router'
+
+const MainLayout = () => {
+  return (
+    <div>
+
+
+        <Outlet />
+
+
+    </div>
+  )
+}
+
+export default MainLayout
