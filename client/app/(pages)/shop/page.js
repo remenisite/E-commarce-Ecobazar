3ef2864804938a4af60e5f6f0d290c3b84d7971e
antor94 <antorremeni@gmail.com>
2026-03-17 19:16:40 +0600

    change code

--- client/app/(pages)/shop/page.js
+++ client/app/(pages)/shop/page.js
@@ -1,31 +1,23 @@
-import LeftCom from '@/app/components/shop/LeftCom'
-import SearchHeader from '@/app/components/shop/SearchHeader'
-import ShopPages from '@/app/components/shop/ShopRight/ShopPages'
-import Breadcrumbs from '@/app/components/utils/Breadcrumbs'
-import React from 'react'
+import LeftCom from "@/app/components/shop/LeftCom";
+import SearchHeader from "@/app/components/shop/SearchHeader";
+import ShopPages from "@/app/components/shop/ShopRight/ShopPages";
+import Breadcrumbs from "@/app/components/utils/Breadcrumbs";
+import React from "react";
 
-const  Shop = () => {
+const Shop = () => {
   return (
     <>
-    
-    <Breadcrumbs h2={"Vegetables"} />
-    <div className='container'>
+      <Breadcrumbs h2={"Vegetables"} />
+      <div className="container">
+        <SearchHeader />
 
-    <SearchHeader />
-
-<div className='flex justify-between gap-[24px]' >
-  
-    <LeftCom />
-    <ShopPages />
-</div>
-    
-    
-    
-    
-    
-    </div>
+        <div className="flex justify-between gap-[24px]">
+          <LeftCom />
+          <ShopPages />
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default  Shop
+export default Shop;
