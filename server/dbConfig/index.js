d7a14a50611b8fa948cafbcb5082a28999495b1d
antor94 <antorremeni@gmail.com>
2026-04-05 23:36:18 +0600

    backend remove

--- server/dbConfig/index.js
+++
@@ -1,9 +0,0 @@
-const { default: mongoose } = require("mongoose");
-// node2501-ecommerce
-const dbConfig = () => {
-  return mongoose.connect(process.env.DB_URL).then(() => {
-    console.log("DB Connected");
-  });
-};
-
-module.exports = dbConfig;
--- server/router/order.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const { checkOut } = require("../controllers/orderController");
-const route = express.Router();
-
-route.post("/checkout", checkOut)
-
-module.exports = route;
