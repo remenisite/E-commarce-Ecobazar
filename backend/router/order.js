4c1d48207f26a74c385fe9ad8324b66e64c6ab45
antor94 <antorremeni@gmail.com>
2026-04-10 23:57:57 +0600

    remove backend

--- backend/router/order.js
+++
@@ -1,8 +0,0 @@
-
-const express =require('express')
-const { checkOut } = require('../Controllers/orderController')
-const routee =express.Router()
-
-routee.post('/checkout', checkOut)
-
-module.exports=routee
