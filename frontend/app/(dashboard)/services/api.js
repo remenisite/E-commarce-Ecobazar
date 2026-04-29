6480a2621bef29cba65d202a867b7f9a97a26e31
antor94 <antorremeni@gmail.com>
2026-04-29 10:35:31 +0600

    UPDATE CODe

--- frontend/app/(dashboard)/services/api.js
+++ frontend/app/(dashboard)/services/api.js
@@ -2,16 +2,27 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const adminApiService = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:8000/",
+    baseUrl: "http://localhost:8000",
+    credentials: "include",
   }),
   endpoints: (build) => ({
     getProducts: build.query({
-      query: () => "product/allproducts",
+      query: () => "/product/allproducts",
     }),
     getCategories: build.query({
       query: () => "/category/all",
     }),
+    createNewProduct: build.mutation({
+      query: (productData) => ({
+        url: "/product/create",
+        method: "POST",
+        headers: { "Content-Type": "multipart/form-data" },
+        body: productData,
+      }),
+    }),
   }),
 });
 
-export const { useGetProductsQuery ,useGetCategoriesQuery } = adminApiService;
+export const { useGetProductsQuery, useGetCategoriesQuery, useCreateNewProductMutation } = adminApiService;
+
+// getProducts = http://localhost:8000/products
