0e85e3ac9db793ccdfbfbf3eb335279cecbf73a2
antor94 <antorremeni@gmail.com>
2026-05-11 13:50:32 +0600

    update product.js

--- backend/router/product.js
+++ backend/router/product.js
@@ -1,5 +1,37 @@
 const express = require("express");
-
+const {
+  createProduct,
+  getProductList,
+  getProductDetals,
+  updateProduct,
+} = require("../controllers/productController");
+const authMiddleWare = require("../middleware/authMiddleware");
+const roleCheckMiddleware = require("../middleware/roleCheckMiddleware");
+const multer = require("multer");
 const route = express.Router();
+const upload = multer();
+
+route.post(
+  "/create",
+  authMiddleWare,
+  roleCheckMiddleware("admin", "editor"),
+  upload.fields([
+    { name: "thumbnail", maxCount: 1 },
+    { name: "images", maxCount: 4 },
+  ]),
+  createProduct,
+);
+route.get("/allproducts", getProductList);
+route.get("/:slug", getProductDetals);
+route.put(
+  "/update/:slug",
+  authMiddleWare,
+  roleCheckMiddleware("admin", "editor"),
+  upload.fields([
+    { name: "thumbnail", maxCount: 1 },
+    { name: "images", maxCount: 4 },
+  ]),
+  updateProduct,
+);
 
 module.exports = route;
